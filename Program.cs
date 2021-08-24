f79637f34a3ef1c90bd6a9ee0c9b71c4a9244227
PirkkoSutinen <pirkko.sutinen@korkeela.com>
2021-08-24 11:10:14 +0300

    Update Program.cs
    Muutos käyttäjälle menevässä kommentissa.

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace tehtävä1
             if(tulos)
             {
                 Console.WriteLine($"Ajattele {nimi.ToUpper()}, vuonna 2031 olet jo {iika + 10} vuotta vanha");
-                Console.WriteLine("Siis tosi vanha.");
+                Console.WriteLine("Ihan nuori siis");
             }
             else
             {
