5cc66879d1963337168baf697ee6868c6bfebb79
VeetiTurunen <veeti.turunen@edu.sasky.fi>
2023-02-03 09:55:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace tehtävä1
         {
             Console.WriteLine("Mikä sinun nimesi on?");
             string nimi = Console.ReadLine();
-            Console.WriteLine("Minä ikäinen olet?");
-            string ika = Console.ReadLine();
+            Console.WriteLine("Mikä sinun ikäsi on?");
+            string ikaString = Console.ReadLine();
             bool tulos;
-            int iika;
-            tulos = int.TryParse(ika, out iika);
-            if(tulos)
+            int ika;
+            tulos = int.TryParse(ikaString, out ika);
+            if (tulos)
             {
                 Console.WriteLine($"Ajattele {nimi.ToUpper()}, vuonna 2031 olet jo {ika + 10} vuotta vanha");
             }
@@ -27,4 +27,6 @@ namespace tehtävä1
             }
         }
     }
+}
+
 }
