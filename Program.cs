cd296176fd40850614012ce5dea62dbc24363c3b
JuhoJoona <74930765+JuhoJoona@users.noreply.github.com>
2024-01-31 09:36:02 +0200

    Korjaa kirjoitusvirheet ja korjaa mahdolliset null viittaukset

--- Program.cs
+++ Program.cs
@@ -11,15 +11,15 @@ namespace tehtävä1
         static void Main(string[] args)
         {
             Console.WriteLine("Mikä sinun nimesi on?");
-            string nimi = Console.ReadLine();
-            Console.WriteLine("Minä ikäinen olet?");
-            string ika = Console.ReadLine();
+            string? nimi = Console.ReadLine();
+            Console.WriteLine("Minkä ikäinen olet?");
+            string? ika = Console.ReadLine();
             bool tulos;
             int iika;
             tulos = int.TryParse(ika, out iika);
             if(tulos)
             {
-                Console.WriteLine($"Ajattele {nimi.ToUpper()}, vuonna 2031 olet jo {ika + 10} vuotta vanha");
+                Console.WriteLine($"Ajattele {nimi?.ToUpper()}, vuonna 2031 olet jo {iika + 10} vuotta vanha");
             }
             else
             {
